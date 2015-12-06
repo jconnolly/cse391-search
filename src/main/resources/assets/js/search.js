f6db638ae8cfa375bd34d5e5881b183acb065618
Celeste <celeste.q.ma@hotmail.com>
2015-12-05 23:31:01 -0500

    Frontend side added

--- src/main/resources/assets/js/search.js
+++ src/main/resources/assets/js/search.js
@@ -1,18 +1,51 @@
 $(document).ready( function() {
-  $("#search").click(function () {
+    
+    $("#go-button").on("click", searchResults);
 
-    var query = $("#search").val();
-    $.ajax({
-      url: "/API/"+title,
-      data: data,
-      async: false,
-      cache: false,
-      contentType: false,
-      processData: false,
-      type: 'POST'
-    })
-  });
 });
 
+function goButtonOnClick() {
+    var query = $("#search-box").val();
+    if (query) {
+        $.ajax({
+            url: "/API/"+ query,
+            //data: data,
+            async: false,
+            //cache: false,
+            //contentType: false,
+            //processData: false,
+            type: 'post',
+            datatype: "json",
+            success: searchResultsOnSuccess,
+            error: searchResultsOnError,
+            complete: searchResultsOnComplete
+        })
+    }
+}
 
+function searchResults() {
+    $.ajax({
+        dataType: "json",
+        url: "../json/searchResults.json",
+        //data: data,
+        success: searchResultsOnSuccess
+    });
+}
+function searchResultsOnSuccess(items){
+    $.each(items, function(index, value) {
+        var result = JSON.parse(value);
+        $(".page-name").text(result["pageName"]);
+        $(".page-url").href(result["pageUrl"]);
+        $(".page-snippet").text(result["pageSnippet"]);
+    });
+}
+
+function searchResultsOnError(error) {
+    var errorSpan = "<span>" + error + "</span>";
+    $("#search-form").append(errorSpan);
+}
+
+function searchResultsOnComplete() {
+    // DO nothing
+}
 
