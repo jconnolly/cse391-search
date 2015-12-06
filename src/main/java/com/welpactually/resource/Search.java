ca19404ff4eaf3e1eadd5e056bc01fdfa2ee4c0c
John Connolly <jeconnol@gmail.com>
2015-12-05 21:16:32 -0500

    moving right along...

--- src/main/java/com/welpactually/resource/Search.java
+++ src/main/java/com/welpactually/resource/Search.java
@@ -1,19 +1,26 @@
 package com.welpactually.resource;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.welpactually.view.Results;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 @Path("/")
 public class Search {
 
 
     private final SolrClient solrClient;
+    private ObjectMapper mapper = new ObjectMapper();
+    private org.slf4j.Logger logger = LoggerFactory.getLogger(Search.class);
 
     public Search(SolrClient solr) {
         this.solrClient = solr;
@@ -24,7 +31,19 @@ public class Search {
     @Produces(MediaType.TEXT_HTML)
     public Response search(@QueryParam("q") String q) throws IOException, SolrServerException {
         SolrQuery query = new SolrQuery(q);
-        solrClient.query(query);
-        return Response.ok((q)).build();
+        QueryResponse result = solrClient.query(query);
+        logger.info(result.toString());
+        return Response.ok(mapper.writeValueAsString(result.getResults())).build();
+    }
+
+    @GET
+    @Path("/results")
+    @Produces(MediaType.TEXT_HTML)
+    public Results searchHtml(@QueryParam("q") String q) throws IOException, SolrServerException {
+        SolrQuery query = new SolrQuery(q);
+        QueryResponse result = solrClient.query(query);
+        logger.info(result.toString());
+        return new Results(result.getResults());
+
     }
 }
--- src/main/java/com/welpactually/view/Results.java
+++ src/main/java/com/welpactually/view/Results.java
@@ -1,9 +1,15 @@
 package com.welpactually.view;
 
 import io.dropwizard.views.View;
+import org.apache.solr.common.SolrDocumentList;
 
 public class Results extends View {
     public Results() {
+        super("results.mustache");
+    }
+
+
+    public Results(SolrDocumentList results) {
         super("results.mustache");
     }
 }
